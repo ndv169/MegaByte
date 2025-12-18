877c5a332b4a1d99ebcc3b7861c0ab9c33a30381
marco <marco@marco007.dev>
2025-12-18 13:26:35 +0100

    feat

--- opdracht-9/main.c
+++ opdracht-9/main.c
@@ -1,6 +1,31 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+bool isPalindrome(const char *str) {
+    int left = 0;
+    int right = strlen(str) - 1;
+
+    while (left < right) {
+        if (str[left] != str[right]) {
+            return false;
+        }
+        left++;
+        right--;
+    }
+    return true;
+}
 
 int main() {
-    printf("hello kitty");
+    char input[690];
+    printf("Woord:\n");
+    scanf("%69s", input);
+
+    if(isPalindrome(input)) {
+        printf("dit woord is een palindroom!");
+    } else {
+        printf("dit is geen palindroom!!!!!");
+    }
+
     return 0;
 }
